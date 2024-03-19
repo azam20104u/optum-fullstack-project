71380c90fc96c8255518aea7bd051f4e0849901b
azam20104u <azam20104u@gmail.com>
2024-03-20 03:53:25 +0530

    change return statement with sort and efficient

--- ms/doctor/src/main/java/com/optum/doctor/controller/DoctorController.java
+++ ms/doctor/src/main/java/com/optum/doctor/controller/DoctorController.java
@@ -3,7 +3,6 @@ package com.optum.doctor.controller;
 import com.optum.doctor.model.Doctor;
 import com.optum.doctor.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,30 +17,35 @@ public class DoctorController {
     @PostMapping("/")
     public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor doctor) {
         Doctor savedDoctor = doctorService.saveDoctor(doctor);
-        return new ResponseEntity<Doctor>(savedDoctor, HttpStatusCode.valueOf(200));
+//        return new ResponseEntity<Doctor>(savedDoctor, HttpStatusCode.valueOf(200));
+        return ResponseEntity.ok(savedDoctor);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Doctor> getDoctor(@PathVariable int id) {
         Doctor doctor = doctorService.getDoctor(id);
-        return new ResponseEntity<Doctor>(doctor, HttpStatusCode.valueOf(200));
+//        return new ResponseEntity<Doctor>(doctor, HttpStatusCode.valueOf(200));
+        return ResponseEntity.ok(doctor);
     }
 
     @GetMapping("/")
     public ResponseEntity<List<Doctor>> getDoctors() {
         List<Doctor> list = doctorService.getDoctors();
-        return new ResponseEntity<List<Doctor>>(list, HttpStatusCode.valueOf(200));
+//        return new ResponseEntity<List<Doctor>>(list, HttpStatusCode.valueOf(200));
+        return ResponseEntity.ok(list);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Doctor> updateDoctor(@PathVariable int id, @RequestBody Doctor doctor) {
         Doctor updatedDoctor = doctorService.updateDoctor(id, doctor);
-        return new ResponseEntity<Doctor>(updatedDoctor, HttpStatusCode.valueOf(200));
+//        return new ResponseEntity<Doctor>(updatedDoctor, HttpStatusCode.valueOf(200));
+        return ResponseEntity.ok(updatedDoctor);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteDoctor(@PathVariable int id) {
         doctorService.deleteDoctor(id);
-        return new ResponseEntity<String>("Doctor Deleted", HttpStatusCode.valueOf(200));
+//        return new ResponseEntity<String>("Doctor Deleted", HttpStatusCode.valueOf(200));
+        return ResponseEntity.ok("Doctor Deleted");
     }
 }
